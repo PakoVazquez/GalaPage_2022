b6d5ce2741093f54ae2f44937a66481256f0b547
PakoVazquez <franciscovazquezitd@gmail.com>
2023-07-11 17:48:23 -0700

    Inicio de archivos de traducción, carpeta recursos

--- GalaPage_2022/Program.cs
+++ GalaPage_2022/Program.cs
@@ -1,4 +1,10 @@
+using Microsoft.AspNetCore.Mvc.Razor;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//Soporte para global y local
+builder.Services.AddLocalization(option => { option.ResourcesPath = "Recursos"; });
+builder.Services.AddMvc().AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix).AddDataAnnotationsLocalization();
 
 // Add services to the container.
 builder.Services.AddRazorPages();
@@ -19,6 +25,15 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
+
+// configurar lenguaje
+var lenguajesSoportados = new[] { "es", "en" };
+var opcionesLocalizacion = new RequestLocalizationOptions()
+                .SetDefaultCulture(lenguajesSoportados[0])
+                .AddSupportedCultures(lenguajesSoportados)
+                .AddSupportedUICultures(lenguajesSoportados);
+
+app.UseRequestLocalization(opcionesLocalizacion);
 
 app.MapRazorPages();
 
